9ffa56ccf1c43fb6b58cc3593dbc5cc722f69bac
ssujaewoo <astec98@naver.com>
2017-05-19 16:50:00 +0900

    hi

---
+++ a.c
@@ -0,0 +1,34 @@
+
+#include 
+int main(void)
+{
+ int grade[10];
+ int i, j, sum, tmp;
+ float average;
+ //성적입력
+for (i = 0; i < 10; i++) {
+ printf("%d번째 성적을 입력하세요:", i);
+ scanf("%d", &grade[i]);
+ }
+
+ //평균계산
+for (i = sum = 0; i < 10; i++)
+ sum+=grade[i];
+ average = sum / 10.0;
+ print("성적 평균: %f\n", average);
+
+ //선택정렬: 버블정렬
+for (i = 0; i < 9; i++)
+ for(j = 9; j > i; j--)
+ if (grade[j-1] > grade[j]){
+ tmp = grade[j-1];
+ grade[j-1] = grade[j];
+ grade[j] = tmp;
+ }
+ //성적순으로 출력
+print("성적순:");
+for(i=0; i<10; i++)
+ printf("%d", grade[i]);
+ printf("\n");
+ return 0;
+}
